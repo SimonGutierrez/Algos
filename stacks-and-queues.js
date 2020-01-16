c7124aca133873b0361a9e0f7f022651afdfa9cd
SimonG <saguti15@g.holycross.edu>
2020-01-16 14:23:16 -0500

    add: added stacks and queues to algos

---
+++ stacks-and-queues.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+    }
+
+    push(val) {
+        const newTop = new Node(val);
+        newTop.next = this.top;
+        this.top = newTop;
+
+        return this;
+    }
+
+    pop() {
+        if (!this.top) return null;
+
+        const nodeToRemove = this.top;
+        this.top = nodeToRemove.next;
+        nodeToRemove.next = null;
+
+        return nodeToRemove;
+    }
+}
+
+const testStack = new Stack;
+
+console.log(testStack.push(1)) // obj: {this.top: node{val: 1, nxt: null}}
+console.log(testStack.push(2)) // obj: {this.top: node{val: 2, nxt: node{val: 1, nxt: null}}}
+console.log(testStack.pop()) // obj: node{val: 2, nxt: null}
+console.log(testStack.push(3)) // obj: {this.top: node{val: 3, nxt: node{val: 1, nxt: null}}}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    push(val) {
+
+    }
+}
