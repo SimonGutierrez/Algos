62ec234fd54b1c4e3d0e1eaac7ddff0c23b71e10
SimonG <saguti15@g.holycross.edu>
2020-01-21 15:56:31 -0500

    add: added python debugging questions

--- python-pract.py
+++ python-pract.py
@@ -1,2 +1,91 @@
-print("Hello World!")
+# debugging
 
+# Pirates
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting in ["Arrr!"): 
+    print("Go away, pirate.")
+elif
+print("Greetings, hater of pirates!")
+
+# correct code
+
+
+# Collections
+authrs = {
+    "Charles Dickens": "1870",
+    "William Thackeray": "1863",
+    "Anthony Trollope": "1882",
+    "Gerard Manley Hopkins": "1889"
+
+for author date in authors.items{}:
+    print "%s" % authors + " died in " + "%d." % Date
+}
+
+# correct code
+
+# Branching
+year == int.input("Greetings! What is your year of origin? '))
+
+if year <= 1900
+    print ('Woah, that's the past!')
+elif year > 1900 && year < 2020:
+    print ("That's totally the present!")
+elif:
+    print ("Far out, that's the future!!")
+
+# correct code
+
+# Classes
+classy Person:
+  def __initalize__(self, first_name, last_name):
+    self.first = first_name
+    self.last = lname
+  def speak(self):
+  print("My name is + " self.fname + " " + self.last)
+
+me = Person("Brandon", "Walsh")
+you = Person("Ethan", "Reed")
+
+me.speak()
+you.self.speak
+
+# correct code
+
+# Grading
+exam_one = int(input("Input exam grade one: "))
+
+exam_two = input("Input exam grade two: "))
+
+exam_3 = str(input("Input exam grade three: "))
+
+grades = [exam_one exam_two exam_three]
+sum = 0
+for grade in grade:
+  sum = sum + grade
+
+avg = sum / len(grdes)
+
+if avg >= 90:
+    letter_grade = "A"
+elif avg >= 80 and avg < 90
+    letter_grade = "B"
+elif avg > 69 and avg < 80:
+    letter_grade = "C'
+elif avg <= 69 and avg >= 65:
+    letter_grade = "D"
+elif:
+    letter_grade = "F"
+
+for grade in grades:
+    print("Exam: " + str(grade))
+
+    print("Average: " + str(avg))
+
+    print("Grade: " + letter_grade)
+
+if letter-grade is "F":
+    print "Student is failing."
+else:
+    print "Student is passing."
+
+# correct code
