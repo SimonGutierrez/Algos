2ee05ad28737bf5ed7ed1c942f23be0c64252a48
SimonG <saguti15@g.holycross.edu>
2020-01-25 13:28:12 -0500

    finished palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,15 +1,23 @@
 function isPalindrome(string) {
     let reverse = '';
-    let midPt = Math.ceil(string.length / 2);
+    let endHalf = '';
+    let midPt = Math.floor(string.length / 2);
 
-    for (let i = 0; i < string.length; i++) {
+    for (let i = 0; i < midPt; i++) {
         reverse = string[i] + reverse;
     }
 
-    if (reverse === string) return true;
+    if (string.length % 2 === 0 ) {
+        endHalf = string.slice(midPt, string.length);
+    } else {
+        endHalf = string.slice(midPt + 1, string.length);
+    }
+
+    if (reverse === endHalf) return true;
 
     return false;
 }
 
 console.log(isPalindrome('tacocat')) // true
+console.log(isPalindrome('poop')) // true
 console.log(isPalindrome('notpalindrome')) // false
