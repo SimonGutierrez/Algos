1472d55622fa6743c7c74d3ebe1ee3fc085a4da2
SimonG <saguti15@g.holycross.edu>
2020-01-26 18:33:08 -0500

    finished ceaser

--- ceasorCipher.js
+++ ceasorCipher.js
@@ -14,16 +14,19 @@ function caesarCipherEncryptor(string, key) {
 
 function helper(alphabet, letter, key) {
     let index = alphabet.indexOf(letter);
+    let newindex = index + key ;
 
-    if (index === 25) {
-        index = -1;
+    if (newindex > 25) {
+        newindex = newindex % 25 - 1;
     }
 
-    const newLetter = alphabet[index + key]
+    const newLetter = alphabet[newindex]
 
     return newLetter;
 }
 
   console.log(caesarCipherEncryptor('abc', 2)) // cde
   console.log(caesarCipherEncryptor('xyz', 2)) // zab
-
+  console.log(caesarCipherEncryptor('abc', 3)) // def
+  console.log(caesarCipherEncryptor('xyz', 5)) // cde
+  console.log(caesarCipherEncryptor('abc', 52)) // abc
