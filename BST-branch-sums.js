dab29bbc3a96a71ae6b75554a17e25a21500f058
SimonG <saguti15@g.holycross.edu>
2020-01-26 22:44:41 -0500

    finished BST branch sums

--- BST-branch-sums.js
+++ BST-branch-sums.js
@@ -6,7 +6,29 @@ class BinaryTree {
     }
   }
 
-  function branchSums(root, sums) {
-    let sum = 0;
+  function branchSums(root) {
+    let sumsArray = []
+
+    findBranchSums(root, sumsArray, 0)
+
+    return sumsArray;
+  }
+
+  const findBranchSums = (node, array, sum) => {
+    if (!node) return;
+
+		const newSum = sum + node.value
+
+    if (!node.left && !node.right) {
+      array.push(newSum);
+      return;
+    }
+
+    findBranchSums(node.left, array, newSum);
+		findBranchSums(node.right, array, newSum);
 
   }
+
+  // Time = O(n) # of nodes;
+  // Space = O(n) log(n) for statndard BST but also keeping track of leaves in array so about 1/2 of the BST;
+
