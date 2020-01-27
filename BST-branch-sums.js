d739b0d0bb788c50f131d223679c0cae4713ce81
SimonG <saguti15@g.holycross.edu>
2020-01-26 20:34:23 -0500

    added two bst problems

---
+++ BST-branch-sums.js
@@ -0,0 +1,12 @@
+class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  function branchSums(root, sums) {
+    let sum = 0;
+
+  }
