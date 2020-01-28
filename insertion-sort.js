e4113703cc7eac4b6cac8a4e237cb41eb2819036
SimonG <saguti15@g.holycross.edu>
2020-01-28 16:03:37 -0500

    added test specs

--- insertion-sort.js
+++ insertion-sort.js
@@ -15,4 +15,6 @@ function insertionSort(array) {
     return array;
   }
 
+  console.log(insertionSort([2, 1, 3])) // [1, 2, 3]
+  console.log(insertionSort([2, 1, 3, 5, 4])) // [1 , 2, 3, 4, 5]
   console.log(insertionSort([8, 5, 2, 9, 5, 6, 3])) // [2, 3, 5, 5, 6, 8, 9]
