23e95914db0b653b4e2f6e89cd2ad89f03d3fd8e
SimonG <saguti15@g.holycross.edu>
2020-01-28 13:59:34 -0500

    finished DLL

--- doubly-linkedlist.js
+++ doubly-linkedlist.js
@@ -8,138 +8,122 @@ class StartNode {
 
   class DoublyLinkedList {
     constructor() {
-      this.head = null;
-      this.tail = null;
-    }
+        this.head = null;
+        this.tail = null;
+      }
 
-    setHead(node) {
-        if (!this.head) {
-            this.head = node;
-            this.tail = node;
-        } else {
-            this.insertBefore(this.head, node)
-        }
-    }
+      setHead(node) {
+          if (this.head === null) {
+              this.head = node;
+              this.tail = node;
 
-    setTail(node) {
-        if (!this.tail) {
-            this.head = node;
-            this.tail = node;
-        } else {
-            this.insertAfter(this.tail, node)
-        }
-    }
+              return;
+          }
 
-    insertBefore(node, nodeToInsert) {
-      let currNode = this.head;
+          this.insertBefore(this.head, node)
 
-      while (currNode) {
-          if (currNode.value === node.value) {
-              if (!currNode.prev) {
-                  nodeToInsert.next = this.head;
-                  this.head = nodeToInsert;
-              } else {
-                  nodeToInsert.prev = currNode.prev;
-                  nodeToInsert.next = currNode;
-                  currNode.prev.next = nodeToInsert;
-                  currNode.prev = nodeToInsert;
-              }
+      }
+
+      setTail(node) {
+          if (this.tail === null) {
+              this.setHead(node);
+
+              return;
+          }
+
+          this.insertAfter(this.tail, node)
+
+      }
+
+      insertBefore(node, nodeToInsert) {
+          if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
+
+          this.remove(nodeToInsert); // remove the node just incase its already in the LL;
+          nodeToInsert.prev = node.prev;
+          nodeToInsert.next = node;
+
+          if (node.prev === null) {
+              this.head = nodeToInsert;
           } else {
+              node.prev.next = nodeToInsert;
+          }
+
+          node.prev = nodeToInsert;
+      }
+
+      insertAfter(node, nodeToInsert) {
+          if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
+
+          this.remove(nodeToInsert); // remove the node just incase its already in the LL;
+          nodeToInsert.prev = node;
+          nodeToInsert.next = node.next;
+
+          if (node.next === null) {
+              this.tail = nodeToInsert;
+          } else {
+              node.next.prev = nodeToInsert;
+          }
+
+          node.next = nodeToInsert;
+      }
+
+      insertAtPosition(position, nodeToInsert) {
+          if (position === 1) {
+              this.setHead(nodeToInsert);
+
+              return;
+          }
+
+          let currNode = this.head;
+          let currPosition = 1;
+
+          while (currNode !== null && currPosition !== position) {
               currNode = currNode.next;
+              currPosition = currPosition + 1;
+          }
+
+          if (currNode !== null) {
+              this.insertBefore(currNode, nodeToInsert);
+          } else {
+              this.setTail(nodeToInsert);
+          }
+
+      }
+
+      removeNodesWithValue(value) {
+          let currNode = this.head;
+          while (currNode) {
+              const nodeToRemove = currNode;
+              currNode = currNode.next;
+
+              if (nodeToRemove.value === value) {
+                  this.remove(nodeToRemove);
+              }
           }
       }
-    }
 
-    insertAfter(node, nodeToInsert) {
-        let currNode = this.head;
+      remove(node) {
+          if (node === this.head) this.head = this.head.next;
+          if (node === this.tail) this.tail = this.tail.prev;
+          this.removeNodeBindings(node);
+      }
 
-        while (currNode) {
-            if (currNode.value === node.value) {
-                if (!currNode.next) {
-                    nodeToInsert.prev = this.tail;
-                    this.tail = nodeToInsert;
-                } else {
-                    nodeToInsert.prev = currNode;
-                    nodeToInsert.next = currNode.next;
-                    currNode.next.prev = nodeToInsert;
-                    currNode.next = nodeToInsert;
-                }
-            } else {
-                currNode = currNode.next;
-            }
-        }
-    }
+      containsNodeWithValue(value) {
+          let currNode = this.head;
 
-    insertAtPosition(position, nodeToInsert) {
-        let currPosistion = 0;
-        let currNode = this.head;
+          while (currNode && currNode.value !== value) {
+              currNode = currNode.next;
+          }
 
-        while (currNode) {
-            if (!currNode.prev) {
-                this.setHead(nodeToInsert);
-            } else if (currPosistion === position) {
-                this.insertBefore(currPosistion, nodeToInsert);
-            } else {
-                currNode = currNode.next;
-                currPosistion++;
-            }
-        }
+          return currNode !== null;
+      }
 
-        this.setTail(nodeToInsert);
-
-    }
-
-    removeNodesWithValue(value) {
-        let currNode = this.head;
-        // does there need to be another temp var?
-        while (currNode) {
-            if (currNode.value === value) {
-                this.remove(currNode);
-            }
-
-            currNode = currNode.next;
-        }
-    }
-
-    remove(node) {
-        let currNode = this.head;
-
-        while (currNode) {
-            if (currNode.value === node.value) {
-                if (!currNode.prev && !currNode.next) {
-                    this.head = null;
-                    this.tail = null;
-                } else if (!currNode.prev) {
-                    this.head = this.head.next;
-                    this.head.prev = null
-                } else if (!currNode.next) {
-                    this.tail = this.tail.prev;
-                    this.tail.next = null;
-                } else {
-                    currNode.prev.next = currNode.next;
-                    currNode.next.prev = currNode.prev;
-                    currNode.prev = null;
-                    currNode.next = null;
-                }
-            } else {
-                currNode = currNode.next;
-            }
-        }
-    }
-
-    containsNodeWithValue(value) {
-        let currNode = this.head;
-
-        while (currNode) {
-            if (currNode.value === value) {
-                return true;
-            } else {
-                currNode = currNode.next;
-            }
-        }
-
-        return false;
-        }
+      removeNodeBindings(node) {
+          if (node.prev) node.prev.next = node.next;
+          if (node.next) node.next.prev = node.prev;
+          node.prev = null;
+          node.next = null;
+      }
   }
 // SEARCHING
 // Time: O(n) - number of nodes;
