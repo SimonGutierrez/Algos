ce9a74c7ba6d3161fcee03a72e98bc8cde077f9e
SimonG <saguti15@g.holycross.edu>
2020-01-30 16:19:14 -0500

    cleaned up code for smalles-diff

--- smallest-diff.js
+++ smallest-diff.js
@@ -1,38 +1,32 @@
 function smallestDifference(arrayOne, arrayTwo) {
-    arrayOne.sort(function (a, b) {
-        return a - b;
-    })
-    arrayTwo.sort(function (a, b) {
-        return a - b;
-    })
+    arrayOne.sort((a, b) => a - b);
+    arrayTwo.sort((a, b) => a - b);
 
     let first = 0;
     let second = 0;
     let abs = Infinity;
-    let pair = [arrayOne[first], arrayTwo[second]];
+    let pair = [];
 
     while (typeof arrayOne[first] === 'number' && typeof arrayTwo[second] === 'number') {
-        let currPosInArrOne = arrayOne[first];
-        let currPosInArrTwo = arrayTwo[second];
-        let currAbs = Math.abs(currPosInArrOne - currPosInArrTwo);
+        let currAbs = Math.abs(arrayOne[first] - arrayTwo[second]);
 
-        if (currPosInArrOne === currPosInArrTwo) {
-            pair = [currPosInArrOne, currPosInArrTwo];
+        if (arrayOne[first] ===  arrayTwo[second]) {
+            pair = [arrayOne[first], arrayTwo[second]];
             return pair;
         }
 
-        if (currPosInArrOne < currPosInArrTwo) {
+        if (arrayOne[first] < arrayTwo[second]) {
             if (currAbs < abs) {
                 abs = currAbs;
-                pair = [currPosInArrOne, currPosInArrTwo]
+                pair = [arrayOne[first], arrayTwo[second]]
             }
             first++;
         }
 
-        if (currPosInArrOne > currPosInArrTwo) {
+        if (arrayOne[first] > arrayTwo[second]) {
             if (currAbs < abs) {
                 abs = currAbs;
-                pair = [currPosInArrOne, currPosInArrTwo]
+                pair = [arrayOne[first], arrayTwo[second]]
             }
             second++;
         }
@@ -41,4 +35,11 @@ function smallestDifference(arrayOne, arrayTwo) {
     return pair;
   }
 
-  console.log(smallestDifference([240, 124, 86, 111, 2, 84, 954, 27, 89], [1, 3, 954, 19, 8]));
+  // Time: O(n*log(n) + m*log(m)) due to the sorting of the arrays, pointing doesnt affect the time.
+  // Space: O(1);
+
+  console.log(smallestDifference([0, 20], [21, -2])); // [20, 21]
+  console.log(smallestDifference([-1, 5, 10, 20, 3], [26, 134, 135, 15, 17])); // [20, 17]
+  console.log(smallestDifference([240, 124, 86, 111, 2, 84, 954, 27, 89], [1, 3, 954, 19, 8])); // [954, 954]
+  console.log(smallestDifference([10, 0, 20, 25, 2000], [1005, 1006, 1014, 1032, 1031])); // [2000, 1032]
+
