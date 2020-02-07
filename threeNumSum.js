d65853d3950971286bd1914d0ea2ef627698a517
SimonG <saguti15@g.holycross.edu>
2020-02-06 19:00:27 -0500

    third attempt

--- threeNumSum.js
+++ threeNumSum.js
@@ -2,19 +2,19 @@ function threeNumberSum(array, targetSum) {
     array.sort((a, b) => a - b);
     let pairs = [];
     let first = 0;
-    let second = Math.floor(array.length / 2);
+    let second = Math.floor((array.length - 1) / 2);
     let third = array.length - 1;
 
     while (first < second) {
         let currSum = array[first] + array[second] + array[third];
 
-        if (currSum === targetSum) {
+        if (currSum > targetSum) {
+            third--;
+        } else if (currSum < targetSum) {
+            first++;
+        } else {
             pairs.push([array[first], array[second], array[third]]);
             second++;
-        } else if (currSum > targetSum) {
-            third--;
-        } else {
-            first++;
         }
     }
 
