66f80f19586c825d087617dc168a0f1df63dbd33
SimonG <saguti15@g.holycross.edu>
2020-02-09 14:33:27 -0500

    finished moveElem to end

--- moveElemToEnd.js
+++ moveElemToEnd.js
@@ -1,3 +1,25 @@
 function moveElementToEnd(array, toMove) {
-    // Write your code here.
+    let left  = 0;
+    let right = array.length - 1;
+
+    while (left < right) {
+        let currLeft = array[left];
+        let currRight = array[right];
+
+        if (currLeft === toMove && currRight === toMove) {
+            right--;
+        } else if (currLeft === toMove && currRight !== toMove) {
+            array[left] = currRight;
+            array[right] = currLeft;
+            left++;
+            right--;
+        } else {
+            left++;
+        }
+    }
+
+    return array;
   }
+
+
+  console.log(moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2))
