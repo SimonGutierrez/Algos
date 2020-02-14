3118c1a01081cee25091556dcda9186078cc41f1
SimonG <saguti15@g.holycross.edu>
2020-02-14 11:49:21 -0500

    started BST traversal

---
+++ BST-Traversal.js
@@ -0,0 +1,11 @@
+function inOrderTraverse(tree, array) {
+    // Write your code here.
+  }
+
+  function preOrderTraverse(tree, array) {
+    // Write your code here.
+  }
+
+  function postOrderTraverse(tree, array) {
+    // Write your code here.
+  }
