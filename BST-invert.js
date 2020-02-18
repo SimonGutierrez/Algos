317f9f7ea7b29cee2844b41917b0401d1783cc8a
SimonG <saguti15@g.holycross.edu>
2020-02-17 22:20:14 -0500

    first attempt on invert tree

--- BST-invert.js
+++ BST-invert.js
@@ -1,3 +1,11 @@
 function invertBinaryTree(tree) {
-    // Write your code here.
+    if (tree === null) {
+        return tree;
+    } else {
+        let temp = tree.left;
+        tree.left = tree.right;
+        tree.right = temp;
+        invertBinaryTree(tree.left);
+        invertBinaryTree(tree.right);
+    }
   }
