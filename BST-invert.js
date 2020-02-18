3f9157c8e5ad05b3748e86b0a023d66a4099025a
SimonG <saguti15@g.holycross.edu>
2020-02-18 13:02:21 -0500

    finished invert BST

--- BST-invert.js
+++ BST-invert.js
@@ -1,11 +1,11 @@
 function invertBinaryTree(tree) {
     if (tree === null) {
         return tree;
-    } else {
+    }
         let temp = tree.left;
         tree.left = tree.right;
         tree.right = temp;
         invertBinaryTree(tree.left);
         invertBinaryTree(tree.right);
-    }
+
   }
