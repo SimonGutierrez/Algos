6d833815bf049c890aeb54c82faed29bf2da5761
SimonG <saguti15@g.holycross.edu>
2020-02-18 16:22:30 -0500

    moved files into folders and added tests to maxSubSet

--- Dynamic/Max-SubSet-Sum-NoAdj.js
+++ Dynamic/Max-SubSet-Sum-NoAdj.js
@@ -19,3 +19,8 @@ function maxSubsetSumNoAdjacent(array) {
 
 // Time : O(n) - where n is the number of elems in the array;
 // Space : O(1) - bc you are not storing any additional data just two variables;
+
+console.log(maxSubsetSumNoAdjacent([])) // 0
+console.log(maxSubsetSumNoAdjacent([1])) // 1
+console.log(maxSubsetSumNoAdjacent([7, 10, 12, 7, 9, 14])) // 33
+console.log(maxSubsetSumNoAdjacent([10, 5, 20, 25, 15, 5, 5, 15, 3, 15, 5, 5, 15])) // 90
