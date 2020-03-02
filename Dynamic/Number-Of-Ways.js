991585a7c9f9cfa63fc1b4e7db53d714b85f3a30
SimonG <saguti15@g.holycross.edu>
2020-03-02 17:20:26 -0500

    Finish: completed Num of ways

--- Dynamic/Number-Of-Ways.js
+++ Dynamic/Number-Of-Ways.js
@@ -1,3 +1,24 @@
 function numberOfWaysToMakeChange(n, denoms) {
-    // Write your code here.
+    let ways = [1]
+    for (let i = 0; i < n; i++) ways.push(0)
+
+    for (let k = 0; k < denoms.length; k++) {
+        let denom = denoms[k];
+        for (let j = 0; j < ways.length; j++) {
+            let currPos = j;
+            if (denom <= currPos) {
+                ways[currPos] += ways[currPos - denom];
+            }
+        }
+    }
+
+    return ways[ways.length - 1];
   }
+
+  // Time: O(n*d) where n is the target and d is the number of denoms we are able to use;
+  // Space: O(n) bc of the array we initiate at the beginning based on the target;
+
+  console.log(numberOfWaysToMakeChange(10, [1, 5, 10, 25])) // 4
+  console.log(numberOfWaysToMakeChange(0, [2, 3, 4, 7])) // 1
+  console.log(numberOfWaysToMakeChange(9, [5])) // 0
+  console.log(numberOfWaysToMakeChange(25, [1, 5, 10, 25])) // 13
