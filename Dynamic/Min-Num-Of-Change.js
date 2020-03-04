16b79e4de6f27df9b1863f97e29fd6c886141133
SimonG <saguti15@g.holycross.edu>
2020-03-04 17:32:59 -0500

    fix: finished and optimized min num of  change

--- Dynamic/Min-Num-Of-Change.js
+++ Dynamic/Min-Num-Of-Change.js
@@ -1,23 +1,27 @@
 function minNumberOfCoinsForChange(n, denoms) {
-   let currTarget = n;
-   let coins = 0;
+    let min = [];
+    for (let i = 0; i <= n; i++) min.push(Infinity);
+    min[0] = 0;
 
-   for (let i = denoms.length - 1; i >= 0; i--) {
-       let denom = denoms[i];
+    for (let j = 0; j < denoms.length; j++) {
+        let denom = denoms[j];
+        for (let k = 0; k < min.length; k++) {
+            if (denom <= k) {
+                if (denom <= k) {
+                    min[k] = Math.min(min[k], 1 + min[k - denom]);
+                }
+            }
+        }
+    }
 
-       if (denom <= currTarget) {
-           coins += Math.floor(currTarget / denom);
-           currTarget = currTarget % denom;
-       }
-   }
-
-   if (currTarget === 0) {
-       return coins;
-   }
-
-   return -1;
+  return min[n] !== Infinity ? min[n] : -1;
 
   }
 
+  // Space = O(n) the length of n for the array min;
+  // Time = O(n*d) the num of denoms and the length of n;
 
-  console.log(minNumberOfCoinsForChange(, [1, 5, 10]))
+  console.log(minNumberOfCoinsForChange(7, [1, 5, 10])) // 3
+  console.log(minNumberOfCoinsForChange(135, [39, 45, 130, 40, 4, 1, 60, 75])) // 2
+  console.log(minNumberOfCoinsForChange(7, [2, 4])) // -1
+  console.log(minNumberOfCoinsForChange(7, [3, 7])) // 1
