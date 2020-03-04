9559df45f20e336c4be8e750c153a4ea988755cb
SimonG <saguti15@g.holycross.edu>
2020-03-04 16:43:44 -0500

    fix: first attempt to min num of change

--- Dynamic/Min-Num-Of-Change.js
+++ Dynamic/Min-Num-Of-Change.js
@@ -1,3 +1,23 @@
 function minNumberOfCoinsForChange(n, denoms) {
-    // Write your code here.
+   let currTarget = n;
+   let coins = 0;
+
+   for (let i = denoms.length - 1; i >= 0; i--) {
+       let denom = denoms[i];
+
+       if (denom <= currTarget) {
+           coins += Math.floor(currTarget / denom);
+           currTarget = currTarget % denom;
+       }
+   }
+
+   if (currTarget === 0) {
+       return coins;
+   }
+
+   return -1;
+
   }
+
+
+  console.log(minNumberOfCoinsForChange(, [1, 5, 10]))
