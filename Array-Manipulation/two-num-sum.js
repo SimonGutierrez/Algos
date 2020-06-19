e44389eafac9f36d0f9c0daf76f007bdb6ff54df
SimonG <saguti15@g.holycross.edu>
2020-06-19 14:12:08 -0400

    fix: made code for two num sum cleaner

--- Array-Manipulation/two-num-sum.js
+++ Array-Manipulation/two-num-sum.js
@@ -1,29 +1,23 @@
 function twoNumberSum(array, targetSum) {
-    array.sort(function (a, b) {
-        return a - b;
-    })
-        let left = 0;
-        let right = array.length - 1;
+	array.sort((a, b) => a - b);
+	let left = 0;
+	let right = array.length - 1;
 
-        while (left < right) {
-            let currSum = array[left] + array[right];
+	while (left < right) {
+		let currSum = array[left] + array[right]
+		if (currSum === targetSum) {
+			return [array[left], array[right]];
+		} else if (currSum > targetSum) {
+			right--;
+		} else {
+			left++;
+		}
+	}
 
-            if (currSum === targetSum) {
-                return [array[left], array[right]]
-            }
-            if (currSum > targetSum) {
-                right--;
-            }
-            if (currSum < targetSum) {
-                left++
-            }
-        }
-
-        return [];
-    }
-
-console.log(twoNumberSum([4, 6, 1], 5).sort((a, b) => a - b)) // [1, 4]
-console.log(twoNumberSum([4, 6, 1, -3], 3).sort((a, b) => a - b)) // [-3, 6]
-console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10).sort((a, b) => a - b)) // [-1, 11]
-console.log(twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17).sort((a, b) => a - b)) // [8, 9]
-console.log(twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 15], 18).sort((a, b) => a - b)) // [3, 15]
+	return [];
+}
+console.log(twoNumberSum([4, 6, 1], 5)) // [1, 4]
+console.log(twoNumberSum([4, 6, 1, -3], 3)) // [-3, 6]
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10)) // [-1, 11]
+console.log(twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17)) // [8, 9]
+console.log(twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 15], 18)) // [3, 15]
