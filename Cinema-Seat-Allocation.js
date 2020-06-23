7044de861ad86b8dcb19fd30f6d4bd1319208d63
SimonG <saguti15@g.holycross.edu>
2020-06-22 20:41:05 -0400

    add: added new problem cinema seat

---
+++ Cinema-Seat-Allocation.js
@@ -0,0 +1,16 @@
+/* eslint-disable comma-spacing */
+// example: what a cinema looks like
+
+let theater = [
+    [1,2,3, 4,5,6,7, 8,9,10],
+    [1,2,3, 4,5,6,7, 8,9,10],
+    [1,2,3, 4,5,6,7, 8,9,10],
+]
+
+// how many families of 4 can fit in each theater
+// rules: families need to be sat together, exception families can be split in pairs of two if split by an aisle
+// example: families can be seated in 4-5-6-7, or 6-7-8-9 and 2-3-4-5 (exceptions)
+
+var maxNumberOfFamilies = function(n, reservedSeats) {
+
+};
