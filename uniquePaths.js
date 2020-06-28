c684cb72c8bf55d1aad0f9e636fe554f3926f588
SimonG <saguti15@g.holycross.edu>
2020-06-28 14:40:46 -0400

    fix: completed unipue paths

--- uniquePaths.js
+++ uniquePaths.js
@@ -1,10 +1,7 @@
 /* eslint-disable complexity */
-const grid = [
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 0, 0]
-  ]
+const grid = [[0, 0], [1, 1], [0, 0]]
 
+// eslint-disable-next-line max-statements
 var uniquePathsWithObstacles = function(obstacleGrid) {
     let row = 1;
     let column = 1;
@@ -14,24 +11,29 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
     }
 
     obstacleGrid[0][0] = 1;
-
+    let clearPath1 = true;
     while (column < obstacleGrid[0].length) {
-        if (obstacleGrid[0][column] !== 1) {
+
+        if (obstacleGrid[0][column] !== 1 && clearPath1) {
             obstacleGrid[0][column] = 1;
         } else {
             obstacleGrid[0][column] = 0;
-        }
+            clearPath1 = false;
 
+        }
+        // console.log(obstacleGrid[0][column]);
         column++;
     }
 
+    let clearPath2 = true;
     while (row < obstacleGrid.length) {
-        if (obstacleGrid[row][0] !== 1) {
+
+        if (obstacleGrid[row][0] !== 1 && clearPath2) {
             obstacleGrid[row][0] = 1;
         } else {
             obstacleGrid[row][0] = 0;
+            clearPath2 = false;
         }
-
         row++;
     }
 
@@ -40,22 +42,22 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
 
 
     while (row < obstacleGrid.length) {
-        while (column < obstacleGrid[row].length) {
-            if (obstacleGrid[row][column] !== 1) {
-                obstacleGrid[row][column] = obstacleGrid[row - 1][column] + obstacleGrid[row][column - 1];
-            } else {
-                obstacleGrid[row][column] = 0;
+        if (obstacleGrid[row]) {
+            while (column < obstacleGrid[row].length) {
+                if (obstacleGrid[row][column] !== 1) {
+                    obstacleGrid[row][column] = obstacleGrid[row - 1][column] + obstacleGrid[row][column - 1];
+                } else {
+                    obstacleGrid[row][column] = 0;
+                }
+                column++;
             }
-            column++;
         }
 
         column = 1;
         row++;
     }
-    column = obstacleGrid[0].length - 1;
-
-    return obstacleGrid[row - 1][column];
-
+    console.log('the grid', obstacleGrid)
+    return obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
 };
 
 console.log(uniquePathsWithObstacles(grid));
