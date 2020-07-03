1e310da5dedf89ec6411a7ad0f4b6d1eaa42dd91
SimonG <saguti15@g.holycross.edu>
2020-07-03 16:33:42 -0400

    fix: completed search rotated

--- leetcode/Search-Rotated-Sorted-Array.js
+++ leetcode/Search-Rotated-Sorted-Array.js
@@ -1,3 +1,29 @@
+/* eslint-disable complexity */
+/* eslint-disable no-unneeded-ternary */
 var search = function(nums, target) {
+    let index = (nums[0] > target) ? nums.length - 1 : 0;
+    let forward = index ? false : true;
 
+    while (nums[index] !== undefined) {
+        if (nums[index] === target) return true;
+
+        if (forward && nums[index] > target) {
+            return false;
+        } else if (!forward && nums[index] < target) {
+            return false;
+        }
+
+        if (nums[index] > target) {
+            index--;
+        } else {
+            index++;
+        }
+    }
+
+    return false;
 };
+
+console.log(search([2, 5, 6, 0, 0, 1, 2], 0)); // true;
+console.log(search([4, 5, 6, 7, 1, 2, 3, 4], 6)); // true;
+console.log(search([1, 1, 3], 3)); // true;
+console.log(search([2, 5, 6, 0, 0, 1, 2], 0)); // false;
