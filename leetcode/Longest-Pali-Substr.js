3a5c5aebcbde79a41527dc82f99f56cebb326cb3
SimonG <saguti15@g.holycross.edu>
2020-07-14 13:25:24 -0400

    fix: finished longest pali substr

---
+++ leetcode/Longest-Pali-Substr.js
@@ -0,0 +1,30 @@
+const longestPalindrome = (s) => {
+    if (s.length === 0 || s === null) return '';
+    let max = '';
+    for (let i = 0; i < s.length; i++) {
+        // this loop is to take into account
+        // different palindromes like 'aba' and 'abba'
+        for (let j = 0; j < 2; j++) {
+            let left = i;
+            let right = i + j;
+            while (s[left] && s[left] === s[right]){
+                right++;
+                left--;
+            }
+            // here imagine we get into string like
+            // "sabbad", then
+            // right = 5
+            // left = 0
+            // and actual length of "abba" should be "4"
+            // 5 - 0 - 1 === 4
+            if ((right - left - 1) > max.length) {
+                max = s.substring(left + 1, right);
+            }
+        }
+    }
+    return max;
+};
+
+console.log(longestPalindrome('sabbad')) // abba
+console.log(longestPalindrome('thetacocat')) // tacocat
+console.log(longestPalindrome('findthe/racecar|longestpali')) // racecar
