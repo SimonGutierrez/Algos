2367016db61d08607f26ca238d88d147d7a00418
SimonG <saguti15@g.holycross.edu>
2020-07-20 23:03:55 -0400

    fix: third attempt to basic calc

--- leetcode/Basic-calc.js
+++ leetcode/Basic-calc.js
@@ -1,51 +1,37 @@
 /* eslint-disable complexity */
+// PEMDAS
 const calculate = (s) => {
     if (s.length === 1) return Number(s);
     let dict = {
-        '+': 1,
+        '+': 2,
         '-': 1,
-        '*': 1,
-        '/': 1
+        '*': 4,
+        '/': 3
     };
     let nums = [];
     let operator = [];
     let substr = '';
 
     for (let i = s.length - 1; i >= 0; i--) {
-        // if (dict[s[i]]) {
-        //     if (nums.length === 2) {
-        //         let op = operator.pop();
-        //         let num2 = Number(nums.pop());
-        //         let num1 = Number(nums.pop());
-        //         nums.push(helper(num1, op, num2));
-        //     }
-        //     operator.push(s[i]);
-        // } else if (Number(s[i]) || Number(s[i]) === 0) {
-        //     substr = s[i] + substr;
-        //     if (dict[s[i - 1]]) {
-        //         nums.push(Number(substr));
-        //         substr = '';
-        //     }
-        // }
         if (nums.length === 2 || i === 0) {
             let num2 = Number(nums.pop());
             let num1 = i === 0 ? Number(s[i] + substr) : Number(nums.pop());
             let op = operator.pop();
-            nums.push(helper(num1, op, num2));
+            num2 ? nums.push(helper(num1, op, num2)) : nums.push(num1);
        } else if (Number(s[i]) || Number(s[i]) === 0) {
             if (dict[s[i - 1]]) {
                 substr = s[i] + substr;
-                nums.push(Number(substr));
+                nums.unshift(Number(substr));
                 substr = '';
-                operator.push(s[i - 1]);
+                operator.unshift(s[i - 1]);
             } else {
                 substr = s[i] + substr;
             }
+            console.log('oper', operator)
     }
-    console.log('nums', nums)
 }
 
-    return nums;
+    return nums[0] < 0 ? nums[0] * -1 :  nums[0];
 }
 
 const helper = (num1, op, num2) => {
@@ -61,4 +47,6 @@ const helper = (num1, op, num2) => {
 }
 
 // white spaces!
-console.log(calculate('3+5')) // 8
+console.log(calculate('10 / 5 + 1 - 10 * 6')) // -57
+// nums = 10, 5, 1, 10, 6
+// op = /, +, -, *
