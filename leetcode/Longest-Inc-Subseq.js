0ce84925de1d4dd7a079577f7e37a8b8a746de0f
SimonG <saguti15@g.holycross.edu>
2020-08-04 19:35:51 -0400

    fix: finished LIS

--- leetcode/Longest-Inc-Subseq.js
+++ leetcode/Longest-Inc-Subseq.js
@@ -1,12 +1,25 @@
+/* eslint-disable complexity */
 const LIS = (nums) => {
-    let count = 1
-    if (!nums.length) return 0;
+    if (nums.length === 0) return 0;
+
+    let dpChart  = new Array(nums.length).fill(1);
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] <= nums[i + 1]) count++;
+        let j = 0;
+        while (j < i) {
+            if (nums[j] < nums[i]) {
+                if (dpChart[i] <= dpChart[j]) dpChart[i] = dpChart[j] + 1;
+            }
+            j++;
+        }
     }
-
-    return count;
+    return dpChart.reduce((a, b) => Math.max(a, b));
 }
+// Time: O(n^2) // looping through the array 2x
+// Space: O(n) // saving a spot for each elem in our dpchart
 
-console.log(LIS([10, 9, 2, 5, 3, 7, 101, 18])) // 4
+console.log(LIS([1, 3, 6, 7, 9, 4, 10])) // 6
+console.log(LIS([0, 1, 2, 3, 4])) // 5
+console.log(LIS([2, 2])) // 1
+console.log(LIS([0])) // 1
+console.log(LIS([])) // 0
