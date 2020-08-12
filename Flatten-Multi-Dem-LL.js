cc7683325590845b3c7c9b44a922a3ea70e492a3
SimonG <saguti15@g.holycross.edu>
2020-08-12 13:55:32 -0400

    add: new problems

---
+++ Flatten-Multi-Dem-LL.js
@@ -0,0 +1,11 @@
+/*
+flatten a doubly linked list
+
+i.e: 1 = 2 = 3 = 10 = null
+              \
+               4 = 5 = 6
+                    \
+                     7 = 8
+
+    ans: 1 = 2 = 3 = 4 = 5 = 7 = 8 = 6 = 10 = null
+*/
