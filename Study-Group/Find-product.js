5aa2a922c13a355d3e29e9b76e77a4ae4130431b
SimonG <saguti15@g.holycross.edu>
2020-08-25 18:46:38 -0400

    fix: attempted flattent multi dimensional LL

--- Study-Group/Find-product.js
+++ Study-Group/Find-product.js
@@ -1,4 +1,4 @@
-// Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+// Given an array nums of n integers where n >= 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
 
 // Example:
 
--- Study-Group/Flatten-Multi-Dem-LL.js
+++ Study-Group/Flatten-Multi-Dem-LL.js
@@ -9,3 +9,31 @@ i.e: 1 = 2 = 3 = 10 = null
 
     ans: 1 = 2 = 3 = 4 = 5 = 7 = 8 = 6 = 10 = null
 */
+
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.prev = null;
+      this.next = null;
+      this.child = null;
+    }
+  }
+
+let LL = new Node(1);
+
+LL.next = new Node(2);
+LL.next.next = new Node(3);
+LL.next.next.next = new Node(10)
+LL.next.next.child = new Node(4)
+LL.next.next.child.next = new Node(5)
+LL.next.next.child.next.child = new Node(7)
+LL.next.next.child.next.child = new Node(8)
+LL.next.next.child.next.next = new Node(6)
+
+console.log(LL);
+
+const flattenLL = (node) => {
+    let stack = [];
+
+
+}
