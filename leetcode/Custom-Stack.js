5eee9e2ba99c8f407e3025ad80f13767f41a2f5f
SimonG <saguti15@g.holycross.edu>
2020-08-31 13:51:53 -0400

    fix: added new problem flood fill and finished custom stack

--- leetcode/Custom-Stack.js
+++ leetcode/Custom-Stack.js
@@ -1,17 +1,31 @@
 // leet code link: https://leetcode.com/problems/design-a-stack-with-increment-operation/
 
 const CustomStack = (maxSize) => {
-
+    this.maxSize = maxSize;
+    this.stack = []
+    return null;
 }
 
 CustomStack.prototype.push = function(x) {
+    if (this.stack.length !== this.maxSize) this.stack.push(x);
 
+    return null;
 };
 
 CustomStack.prototype.pop = function() {
+    if (this.stack.length === 0) return -1;
 
+    return this.stack.pop();
 };
 
 CustomStack.prototype.increment = function(k, val) {
+    if (this.stack.length === 0) return null;
 
+    let cap = k > this.stack.length ? this.stack.length : k
+
+    for (let i = 0; i < cap; i++){
+      this.stack[i] += val
+    }
+
+    return null;
 };
