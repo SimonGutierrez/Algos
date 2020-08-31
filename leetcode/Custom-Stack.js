ef9224d2d25588b1a60fb468a78bf1e09b1964f5
SimonG <saguti15@g.holycross.edu>
2020-08-31 12:05:21 -0400

    add: started new problem custom stack

---
+++ leetcode/Custom-Stack.js
@@ -0,0 +1,17 @@
+// leet code link: https://leetcode.com/problems/design-a-stack-with-increment-operation/
+
+const CustomStack = (maxSize) => {
+
+}
+
+CustomStack.prototype.push = function(x) {
+
+};
+
+CustomStack.prototype.pop = function() {
+
+};
+
+CustomStack.prototype.increment = function(k, val) {
+
+};
