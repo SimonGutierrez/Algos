6ea4d62ea957d8eedaea7b3a23596e9c233aa7b0
SimonG <saguti15@g.holycross.edu>
2020-08-31 20:47:55 -0400

    add: added time complex

--- Study-Group/FloodFill.js
+++ Study-Group/FloodFill.js
@@ -50,4 +50,7 @@ function validNeighbor(image, row, col, originalColor) {
     return false;
 }
 
-console.log(floodFill([[0, 0, 0], [0, 0, 0]], 1, 0, 2));
+// Time = O(n) where n are all elements in the 2D array;
+// Space = O(n) use of stack all points in the 2D array;
+
+console.log(floodFill([[0, 0, 0], [0, 0, 0]], 1, 0, 2)); // [ [ 2, 2, 2 ], [ 2, 2, 2 ] ]
