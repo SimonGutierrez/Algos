15d1d2059fa21c0ac0435a32de6e51c85d61d5e9
SimonG <saguti15@g.holycross.edu>
2020-09-19 12:27:09 -0400

    fix: minor change to parens

--- Dynamic/Permutations.js
+++ Dynamic/Permutations.js
@@ -1,26 +1,26 @@
-// leetcode Link: https://leetcode.com/problems/permutations/discuss/18377/My-simple-Javascript-recursive-solution
+// LeetCode Link: https://leetcode.com/problems/permutations/
 
-var permute = function(nums) {
+const permute = (nums) => {
     let results = [];
 
     let permutations = (current, remaining) => {
-      if (remaining.length <= 0) {results.push(current.slice());}
-      else {
-        for (let i = 0; i < remaining.length; i++) { // Loop through remaining elements
-          current.push(remaining[i]); // Insert the iTH element onto the end of current
-          permutations(current.slice(), remaining.slice(0, i).concat(remaining.slice(i + 1))); // Recurse with inserted element removed
-          current.pop(); // Remove last inserted element for next iteration
-        }
+      if (remaining.length <= 0) {
+          results.push(current.slice());
+        } else {
+            for (let i = 0; i < remaining.length; i++) { // Loop through remaining elements
+                current.push(remaining[i]); // Insert the iTH element onto the end of current
+                permutations(current.slice(), remaining.slice(0, i).concat(remaining.slice(i + 1))); // Recurse with inserted element removed
+                current.pop(); // Remove last inserted element for next iteration
+            }
       }
     };
 
     permutations([], nums);
     return results;
-  };
+};
 
-  console.log(permute([1, 2, 3]))
-
-  /*
+console.log(permute([1, 2, 3]))
+/*
 [
   [1,2,3],
   [1,3,2],
@@ -29,4 +29,4 @@ var permute = function(nums) {
   [3,1,2],
   [3,2,1]
 ]
-  */
+*/
