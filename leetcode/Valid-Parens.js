4cbf0382956d569ff6def242169d2c16944a4875
SimonG <saguti15@g.holycross.edu>
2020-09-19 14:11:55 -0400

    fix: completed leet code version of valid parens

--- leetcode/Valid-Parens.js
+++ leetcode/Valid-Parens.js
@@ -22,7 +22,35 @@ const parens = (n) => {
     return result;
 }
 
-console.log(parens(3)) // [...]
+// console.log(parens(3)) // [...]
 
 
 // https://leetcode.com/problems/valid-parentheses/
+
+const isValid = (s) => {
+    let stack = [];
+    let dict = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (dict[char]) {
+            stack.push(char);
+        } else {
+            let prev = stack.pop();
+            if (dict[prev] !== char) return false;
+        }
+    }
+
+    return stack.length === 0;
+};
+let string1 = '([{[}])', string2 = '([)]', string3 = '()', string4 = '()[]{}';
+
+
+console.log(isValid(string1)) // false;
+console.log(isValid(string2)) // false;
+console.log(isValid(string3)) // true;
+console.log(isValid(string4)) // true;
