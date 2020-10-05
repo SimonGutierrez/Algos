c983213cbf6b55c079be163fe4d3292b7abc2ae7
SimonG <saguti15@g.holycross.edu>
2020-10-05 17:38:27 -0400

    fix: finishes delete dupes II

--- leetcode/No-Dupes.js
+++ leetcode/No-Dupes.js
@@ -1,4 +1,5 @@
 // leet codelink: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+// remove dupes only
 
 class ListNode {
     constructor(value, next) {
@@ -32,4 +33,41 @@ test1.next = new ListNode(2);
 test1.next.next = new ListNode(3);
 test1.next.next.next = new ListNode(3);
 
-console.log(deleteDuplicates(test1))
+console.log(deleteDuplicates(test1)); // 1 -> 2 -> 3
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+// remove all numbers that have dupes
+
+const deleteDuplicatesII = (head) => {
+    let seen = new Set(), currHead = head, noDupes = new ListNode(-1), temp = noDupes;
+
+    while (currHead) {
+        if (!seen.has(currHead.val)) {
+            seen.add(currHead.val);
+            if (currHead.next) {
+                if (!seen.has(currHead.next.val)) {
+                temp.next = currHead;
+                temp = temp.next;
+                }
+            } else {
+                temp.next = currHead;
+                temp = temp.next;
+            }
+        }
+
+        currHead = currHead.next
+    }
+
+    if (temp.next) {
+        if (seen.has(temp.next.val)) temp.next = null;
+    }
+
+    return noDupes.next;
+};
+
+let test2 = new ListNode(1);
+test2.next = new ListNode(2);
+test2.next.next = new ListNode(3);
+test2.next.next.next = new ListNode(3);
+
+console.log(deleteDuplicatesII(test2)); // 1 -> 2
