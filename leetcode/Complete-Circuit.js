281a74bdca1e786680e16b144f58086aede78531
SimonG <saguti15@g.holycross.edu>
2021-01-16 22:19:52 -0500

    fix: added time and space complex also optimal solution

--- leetcode/Complete-Circuit.js
+++ leetcode/Complete-Circuit.js
@@ -29,7 +29,25 @@ const canCompleteCircuit = (gas, cost) => {
     return -1;
 };
 
+// Time: O(N^2) worst case senario look through all possible circuits until the end
+// Space: O(1) no additional space needed
+
 console.log(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])) // 3
 console.log(canCompleteCircuit([2, 3, 4], [3, 4, 3])) // -1
 console.log(canCompleteCircuit([3, 1, 1], [1, 2, 2])) // -1
 
+// O(N) solution O(1) space
+
+var canCompleteCircuit = function(gas, cost) {
+    let curTank = 0, totalTank = 0, pos = 0;
+    for (let i = 0; i < gas.length;i++) {
+        curTank += gas[i] - cost[i];
+        totalTank += gas[i] - cost[i];
+        if (curTank < 0) {
+            curTank = 0;
+            pos = i + 1;
+        }
+    }
+
+    return totalTank < 0 ? -1 : pos;
+}
