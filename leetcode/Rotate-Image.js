a3a30e51262893eb0ec1a78f9fef231a133d37ab
SimonG <saguti15@g.holycross.edu>
2021-02-25 19:48:35 -0500

    fix: finished rotate-image

---
+++ leetcode/Rotate-Image.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/rotate-image/
+
+const matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+const rotate = (matrix) => {
+    let temp = new Array(matrix.length).fill().map(() => new Array(matrix[0].length));
+
+    for (let i = 0; i < matrix.length; i++) {
+        let row = matrix[i];
+        for (let j = 0; j < row.length; j++) {
+            temp[i][j] = matrix[i][j];
+        }
+    }
+
+    let tempRow = temp.length - 1;
+
+    for (let k = 0; k < matrix[0].length; k++) {
+        let col = 0;
+
+        for (let l = 0; l < matrix.length; l++) {
+            matrix[l][k] = temp[tempRow][col];
+            col++;
+        }
+        
+        tempRow--;
+    }
+
+    return matrix;
+};
+
+console.log(rotate(matrix1))
